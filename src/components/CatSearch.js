871f380e6248d2bad623097cdbee84ab73adfe6f
gabgg71 <gabrielaggarcia70@gmail.com>
2022-12-19 13:27:32 -0500

    corregir importacion

--- src/components/CatSearch.js
+++ src/components/CatSearch.js
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { useState } from "react/cjs/react.development";
+import { useState } from "react";
 import { userContext } from "../hooks/userContext";
 
 export const CatSearch = () => {
